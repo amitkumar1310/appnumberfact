7c898922db04ce31217380fbc2bdb9fb2ac5a195
amitkumar1310 <98661988+amitkumar1310@users.noreply.github.com>
2022-10-28 13:04:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const form=document.querySelector('form');
+const factDiv=document.querySelector('.numfact');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const number = e.target.querySelector('input[type="number"]').value;
+    const type=e.target.querySelector('input[type="text"]').value
+ console.log(number,type);
+const loadText='Wait a little bit';
+//const baseurl='http://numbersapi.com/';
+    
+     //fetch(baseurl+number+"/"+type)
+factDiv.innerHTML=loadText;
+ const baseurl='https://cors-anywhere.herokuapp.com/http://numbersapi.com/';
+ fetch(baseurl+number+"/"+type,{
+     method:"GET",
+    headers:{
+       'x-requested-with':'text/plain'
+   }}).then(response =>response.text())
+     .then(text =>factDiv.innerHTML=text);
+}).catch((err)=>console.log(error));
+ 
